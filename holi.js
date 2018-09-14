dd091a3a16996a468ea978ba7e747aee81891c64
Nicolay <yjoya@asesoftware.com>
2018-09-14 09:21:04 -0500

    Second Commit

--- holi.js
+++ holi.js
@@ -7,3 +7,42 @@ function isGay(nombre){
     alert("Resultado de gaycidad :V "+ x);
 }
 
+
+function isGay(nombre){
+    x= false;
+    if (nombre == Dani) {
+        x = true;    
+    }
+    alert("Resultado de gaycidad :V "+ x);
+}
+
+
+
+function isGay(nombre){
+    x= false;
+    if (nombre == Dani) {
+        x = true;    
+    }
+    alert("Resultado de gaycidad :V "+ x);
+}
+
+
+
+function isGay(nombre){
+    x= false;
+    if (nombre == Dani) {
+        x = true;    
+    }
+    alert("Resultado de gaycidad :V "+ x);
+}
+
+
+
+function isGay(nombre){
+    x= false;
+    if (nombre == Dani) {
+        x = true;    
+    }
+    alert("Resultado de gaycidad :V "+ x);
+}
+
